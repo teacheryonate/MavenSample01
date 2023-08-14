ebcf3501451a2876be161299194a06dfadf13c0f
tj705 <a@a.com>
2023-08-14 14:54:49 +0900

    add endpoint

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -17,5 +17,13 @@ public class MainController {
 		
 		return ResponseEntity.ok().body(map);
 	}
+	
+	@GetMapping("/health")
+	public ResponseEntity<?> health(){
+		Map<String, Object> map = new HashMap<>();
+		map.put("stauts", "ok");
+		
+		return ResponseEntity.ok().body(map);
+	}
 
 }
